8b3db052ddd380fe69eba0e0f142c4ccbf54b494
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-08-14 08:52:02 -0400

    f - Add Part 2 tests

--- src/test/kotlin/lacar/junilu/Day19Test.kt
+++ src/test/kotlin/lacar/junilu/Day19Test.kt
@@ -23,13 +23,34 @@ class Day19Test {
                 """.trimIndent().lines()
             ).part1())
         }
+
+        @Test
+        fun `Part 2 example`() {
+            assertEquals(6, Day19.using(
+                """
+                    e => H
+                    e => O
+                    H => HO
+                    H => OH
+                    O => HH
+                    
+                    HOHOHO
+                """.trimIndent().lines()
+
+            ).part2())
+        }
     }
 
     @Nested
     inner class Solution {
         @Test
-        fun `Part 1 -`() {
+        fun `Part 1 - SOLVED`() {
             assertEquals(535, Day19.using(puzzleInput).part1())
+        }
+
+        @Test
+        fun `Part 2 -`() {
+            assertEquals(0, Day19.using(puzzleInput).part1())
         }
     }
 }
