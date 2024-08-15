df12efa1d1eb17d8254273c6ad23d1b96b2ddc41
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-08-14 22:19:15 -0400

    r - Tidy up Day19
    Part 2 still kind of ugly.

--- src/main/kotlin/lacar/junilu/Day19.kt
+++ src/main/kotlin/lacar/junilu/Day19.kt
@@ -10,35 +10,41 @@ class Day19(
     override fun part2() = stepsToReduceToE()
 
     private fun newMolecules() =
-        replacements.flatMap { (left, right) ->
-            molecule.replaceOccurrencesOf(left, right)
+        replacements.flatMap { (element, replacement) ->
+            molecule.replaceOccurrencesOf(element, replacement)
         }
 
     private fun stepsToReduceToE(): Int {
-        val finalRedux = replacements.filter { it.first == "e" }.map { Pair(it.second, it.first) }
-        val intermediateRedux = replacements.filterNot { it.first == "e" }.map { Pair(it.second, it.first) }
-        var newMolecule = molecule
-        var steps = 0
-        while (finalRedux.none { it.first == newMolecule }) {
-            val redux = intermediateRedux.first { newMolecule.contains(it.first) }
-            steps += newMolecule.occurrencesOf(redux.first)
-            newMolecule = newMolecule.replace(redux.first, redux.second)
+        val convertibleToE = replacementsForE()
+        val otherReplacements = otherReplacements()
+        var reducedMolecule = molecule
+        var steps = 1
+        while (reducedMolecule.isNot(convertibleToE)) {
+            val (element, replacement) = otherReplacements.first { (element, _) -> reducedMolecule.contains(element) }
+            steps += reducedMolecule.occurrencesOf(element)
+            reducedMolecule = reducedMolecule.replace(element, replacement)
         }
-        return steps + 1
+        return steps
     }
+
+    private fun replacementsForE() = replacements.filter { it.first == "e" }.map { it.second }
+    private fun otherReplacements() = replacements.filterNot { it.first == "e" }.map { Pair(it.second, it.first) }
 
     companion object {
         fun using(input: List<String>) = Day19(
-            input.dropLast(2).map { line ->
-                val (left, right) = line.split(" => ")
-                Pair(left, right)
-            },
+            input.dropLast(2)
+                .map { it.split(" => ") }
+                .map { (element, replacement) -> Pair(element, replacement) },
             molecule = input.last()
         )
     }
 }
 
-private fun String.occurrencesOf(substring: String) = this.split(substring).size - 1
+private fun String.isNot(reduceableToE: List<String>) =
+    reduceableToE.none { this == it }
+
+private fun String.occurrencesOf(substring: String) =
+    this.split(substring).size - 1
 
 private fun String.replaceOccurrencesOf(left: String, right: String) =
     indices.map { i ->
