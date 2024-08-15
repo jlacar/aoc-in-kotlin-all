81e70659e33bcd3fa3ac818e16c6fe77ffcede65
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-08-15 08:09:18 -0400

    r - Refactor to a more consistent organization of tests and using()

--- src/main/kotlin/lacar/junilu/Day02.kt
+++ src/main/kotlin/lacar/junilu/Day02.kt
@@ -2,10 +2,14 @@ package lacar.junilu
 
 /**
  * Aoc 2015 - Day 2: I Was Told There Would Be No Math
+ *
+ * https://adventofcode.com/2015/day/2
  */
 class Day02(private val boxDimensions: List<IntArray>) : Solution<Int> {
 
     override fun part1() = boxDimensions.sumOf { wrapperNeeded(it) }
+
+    override fun part2() = boxDimensions.sumOf { ribbonNeeded(it) }
 
     private fun wrapperNeeded(dims: IntArray): Int {
         val (w, l, h) = dims
@@ -13,9 +17,15 @@ class Day02(private val boxDimensions: List<IntArray>) : Solution<Int> {
         return areas.sumOf { 2 * it } + areas.min()
     }
 
-    override fun part2() = boxDimensions.sumOf { ribbonNeeded(it) }
-
     private fun ribbonNeeded(dims: IntArray): Int =
         dims.sumOf { 2 * it } - 2 * dims.max() +
         dims.fold(1) { acc, i -> acc * i }
+
+    companion object {
+        fun using(input: List<String>) = Day02(
+            input.map { line ->
+                line.split("x").map { it.toInt() }.toIntArray()
+            }
+        )
+    }
 }
