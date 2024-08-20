69711853402546f99e2933f749b7e7b79a48eafb
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-08-19 22:38:29 -0400

    r - Converted companion object functions to extension functions

--- src/main/kotlin/lacar/junilu/Day11.kt
+++ src/main/kotlin/lacar/junilu/Day11.kt
@@ -10,9 +10,7 @@ class Day11() {
         generateSequence(currentPassword.increment()) { it.increment() }.first { it.isValid() }
 
     private fun String.isValid() =
-        hasNoIllegalCharacters(this) &&
-        hasValidTrio(this) &&
-        hasPairOfDoubleLetters(this)
+        hasNoIllegalCharacters() && hasValidTrio() && hasPairOfDoubleLetters()
 
     private fun String.increment() =
         foldRight(StringBuilder()) { ch, sb ->
@@ -22,21 +20,17 @@ class Day11() {
     private fun Char.nextIncrement(nextOrWrap: Boolean): Char =
         if (nextOrWrap) { if (this != 'z') inc() else 'A' } else { this }
 
+    private fun String.hasNoIllegalCharacters() = none { "ilo".contains(it) }
+
+    private val validTrios = "abcdefghijklmnopqrstuvwxyz".windowed(3).filter { it.hasNoIllegalCharacters() }
+    private fun String.hasValidTrio() = validTrios.any { trio -> contains(trio) }
+
+    private fun String.hasPairOfDoubleLetters() = Regex("([a-z])\\1")
+        .findAll(this, 0)
+        .map { matches -> matches.groupValues.first() }
+        .distinct().count() >= 2
+
+    // Internal, for testing
     internal fun isValid(password: String) = password.isValid()
     internal fun incr(s: String) = s.increment()
-
-    companion object {
-        fun hasNoIllegalCharacters(str: String) = str.none { "ilo".contains(it) }
-
-        private val validTrios = "abcdefghijklmnopqrstuvwxyz".windowed(3).filter { hasNoIllegalCharacters(it) }
-        fun hasValidTrio(password: String) = validTrios.any { trio -> password.contains(trio) }
-
-        private val doubleLetters = Regex("([a-z])\\1")
-        fun hasPairOfDoubleLetters(password: String) = doubleLetters
-            .findAll(password, 0)
-            .map { matches -> matches.groupValues.first() }
-            .distinct().count() >= 2
-
-        // For testing
-    }
 }
