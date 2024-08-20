260319a1cb71f608f939f9f46f831b6cd405671e
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-08-20 12:48:48 -0400

    r - Renamed, reordered

--- src/main/kotlin/lacar/junilu/Day09.kt
+++ src/main/kotlin/lacar/junilu/Day09.kt
@@ -1,30 +1,31 @@
 package lacar.junilu
 
-private typealias SantaRouteSegment = Pair<String, Int>
+private typealias RouteSegment = Pair<String, Int>
 
 /**
  * AoC 2015 - Day 9: All in a Single Night
  *
  *  https://adventofcode/2015/day/10
  */
-class Day09(private val segments: List<SantaRouteSegment>) {
+class Day09(private val segments: List<RouteSegment>) {
 
     private val allCities = segments.flatMap { it.first.split(" to ") }.distinct()
     private val allPossiblePaths = allCities.permutations()
 
-    fun part1() = allPossiblePaths.minOf { eachPath -> distanceThrough(eachPath) }
+    fun shortestRoute() = allPossiblePaths.minOf { eachPath -> distanceThrough(eachPath) }
 
-    fun part2() = allPossiblePaths.maxOf { eachPath -> distanceThrough(eachPath) }
+    fun longestRoute() = allPossiblePaths.maxOf { eachPath -> distanceThrough(eachPath) }
 
-    private fun distanceThrough(cities: List<String>) = cities.windowed(2)
-        .sumOf { (city1, city2) ->
-            segments.find { it.first == "$city1 to $city2" || it.first == "$city2 to $city1" }?.second ?: 0
-        }
+    private fun distanceThrough(path: List<String>) = path.windowed(2)
+        .sumOf { (city1, city2) -> distanceBetween(city1, city2) }
+
+    private fun distanceBetween(city1: String, city2: String) =
+        segments.find { it.first == "$city1 to $city2" || it.first == "$city2 to $city1" }?.second ?: 0
 
     companion object {
         fun using(input: List<String>) = Day09(input.map { segment ->
             val (cities, distance) = segment.split(" = ")
-            SantaRouteSegment(cities, distance.toInt())
+            RouteSegment(cities, distance.toInt())
         })
     }
 }
--- src/test/kotlin/lacar/junilu/Day09Test.kt
+++ src/test/kotlin/lacar/junilu/Day09Test.kt
@@ -1,7 +1,6 @@
 package lacar.junilu
 
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
@@ -10,44 +9,43 @@ private val puzzleInput = readResource("day09")
 
 class Day09Test {
     @Nested
-    inner class Samples {
-        @TestFactory
-        fun `Part 1`() = listOf(
-            """
-            London to Dublin = 464
-            London to Belfast = 518
-            Dublin to Belfast = 141
-            """.trimIndent() to 605,
-        ).map { (segments, expectedShortest) ->
-            DynamicTest.dynamicTest("$segments -> shortest route should be $expectedShortest") {
-                assertEquals(expectedShortest, Day09.using(segments.lines()).part1())
-            }
-        }
-
-        @TestFactory
-        fun `Part 2`() = listOf(
-            """
-            London to Dublin = 464
-            London to Belfast = 518
-            Dublin to Belfast = 141
-            """.trimIndent() to 982,
-        ).map { (segments, expectedLongest) ->
-            DynamicTest.dynamicTest("$segments -> longest route should be $expectedLongest") {
-                assertEquals(expectedLongest, Day09.using(segments.lines()).part2())
-            }
-        }
-    }
-
-    @Nested
     inner class Solution {
         @Test
         fun `Part 1 - SOLVED`() {
-            assertEquals(141, Day09.using(puzzleInput).part1())
+            assertEquals(141, Day09.using(puzzleInput).shortestRoute())
         }
 
         @Test
         fun `Part 2 - SOLVED `() {
-            assertEquals(736, Day09.using(puzzleInput).part2())
+            assertEquals(736, Day09.using(puzzleInput).longestRoute())
+        }
+    }
+
+    @Nested
+    inner class Examples {
+
+        @Test
+        fun `Shortest route`() {
+
+            val segments = """
+            London to Dublin = 464
+            London to Belfast = 518
+            Dublin to Belfast = 141
+            """.trimIndent()
+
+            assertEquals(605, Day09.using(segments.lines()).shortestRoute())
+        }
+
+        @TestFactory
+        fun `Longest route`() {
+
+            val segments = """
+            London to Dublin = 464
+            London to Belfast = 518
+            Dublin to Belfast = 141
+            """.trimIndent()
+
+            assertEquals(982, Day09.using(segments.lines()).longestRoute())
         }
     }
 }
