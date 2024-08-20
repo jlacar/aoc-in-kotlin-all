8e6ae8b3024472ce6da24362a233765635d1ca2a
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-08-20 07:40:45 -0400

    r - Switch to ParameterizedTest

--- src/main/kotlin/lacar/junilu/Day10.kt
+++ src/main/kotlin/lacar/junilu/Day10.kt
@@ -5,11 +5,9 @@ package lacar.junilu
  *
  * https://adventofcode.com/2015/day/10
  */
-class Day10(val input: String = "3113322113") : Solution<Int> {
-    override fun part1(): Int = lookSay(40)
-    override fun part2(): Int = lookSay(50)
-
-    private fun lookSay(n: Int) = (1..n).fold(input) { look, _ -> say(look) }.length
+class Day10() {
+    fun lookSay(input: String, times: Int = 1) =
+        (1..times).fold(input) { number, _ -> say(number) }
 
     private val sameConsecutiveDigit = """(.)\1*""".toRegex()
 
--- src/test/kotlin/lacar/junilu/Day10Test.kt
+++ src/test/kotlin/lacar/junilu/Day10Test.kt
@@ -1,28 +1,24 @@
 package lacar.junilu
 
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class Day10Test {
-    @Nested
-    inner class Samples {
 
-        @TestFactory
-        fun `Part 1`() = listOf(
-            "211" to "1221",
-            "1" to "11",
-            "11" to "21",
-            "21" to "1211",
-            "1211" to "111221",
-            "111221" to "312211"
-        ).map { (input, expected) ->
-            DynamicTest.dynamicTest("$input -> $expected") {
-                assertEquals(expected, Day10(input).say(input))
-            }
-        }
+    @ParameterizedTest(name = "lookSay({0}).times(1) is {1}")
+    @CsvSource(
+        "211, 1221",
+        "1, 11",
+        "11, 21",
+        "21, 1211",
+        "1211, 111221",
+        "111221, 312211"
+    )
+    fun examples(input: String, expected: String) {
+        assertEquals(expected, Day10().say(input))
     }
 
 
@@ -30,12 +26,12 @@ class Day10Test {
     inner class Solution {
         @Test
         fun `Part 1 - SOLVED`() {
-            assertEquals(329356, Day10().part1())
+            assertEquals(329356, Day10().lookSay("3113322113", 40).length)
         }
 
         @Test
         fun `Part 2 - SOLVED`() {
-            assertEquals(4666278, Day10().part2())
+            assertEquals(4666278, Day10().lookSay("3113322113", 50).length)
         }
     }
 }
