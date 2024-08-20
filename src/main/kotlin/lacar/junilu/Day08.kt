7719180f814ad37c950a90e64a9d2fdf69da5667
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-08-20 12:54:36 -0400

    r - Remove Solution interface (not used/needed)

--- src/main/kotlin/lacar/junilu/Day08.kt
+++ src/main/kotlin/lacar/junilu/Day08.kt
@@ -5,15 +5,15 @@ package lacar.junilu
  *
  * https://adventofcode.com/2015/day/8
  */
-class Day08(val input: List<String>) : Solution<Int> {
-    override fun part1(): Int = input.sumOf { it.length - it.decoded().length }
+class Day08(val input: List<String>) {
+    fun part1(): Int = input.sumOf { it.length - it.decoded().length }
 
     private fun String.decoded(): String = drop(1).dropLast(1)
             .let { allQuotes -> Regex("""\\"""").replace(allQuotes, "?") }
             .let { allBackslashes -> Regex("""\\\\""").replace(allBackslashes, "?") }
             .let { allHexCodes -> Regex("""\\x[0-9a-f]{2}""").replace(allHexCodes, "?") }
 
-    override fun part2(): Int = input.sumOf { it.encoded().length - it.length }
+    fun part2(): Int = input.sumOf { it.encoded().length - it.length }
 
     private fun String.encoded(): String = this
             .let { backslashes -> Regex("""\\""").replace(backslashes, """\\\\""") }
