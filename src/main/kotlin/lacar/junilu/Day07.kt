9b5c2b4039b50fe592555f3fb66d50d6bece1263
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-08-21 05:50:09 -0400

    r - Tidy up a bunch: reorder, rename

--- src/main/kotlin/lacar/junilu/Day07.kt
+++ src/main/kotlin/lacar/junilu/Day07.kt
@@ -23,9 +23,9 @@ class Day07(instructions: List<String>) {
         return Circuit(replaceSegmentB(circuit, a)).signalTo("a") ?: throw UnexpectedException("Something's wrong!")
     }
 
-    private fun replaceSegmentB(circuit: Circuit, a: Int): Map<String, SignalProvider> {
+    private fun replaceSegmentB(circuit: Circuit, signal: Int): Map<String, SignalProvider> {
         val newSegments = mutableMapOf<String, SignalProvider>().apply { putAll(circuit.segments) }
-        newSegments["b"] = Value("b", a.toString())
+        newSegments["b"] = Value("b", signal.toString())
         return newSegments
     }
 }
@@ -41,15 +41,8 @@ private val knownSignals = object {
     }
 }
 
-private typealias SegmentMap = Map<String, SignalProvider>
-
-private sealed interface SignalProvider {
-    val name: String
-    fun output(segments: SegmentMap): Int?
-}
-
 private enum class Op {
-    ASSIGN { override fun eval(a: Int, b: Int): Int = a },
+    ASSIGN { override fun eval(a: Int, b: Int): Int = a },                  // b is ignored
     NOT { override fun eval(a: Int, b: Int): Int = a.inv() and 0xffff },    // mask to 16-bit value
     AND { override fun eval(a: Int, b: Int): Int = a and b },
     OR { override fun eval(a: Int, b: Int): Int = a or b },
@@ -59,44 +52,57 @@ private enum class Op {
     abstract fun eval(a: Int, b: Int = 0): Int
 }
 
-/** Extension function to check if value is a numeric literal */
-private fun String.isNumberLiteral(): Boolean = first().isDigit()
+private typealias SegmentMap = Map<String, SignalProvider>
 
-private class Value(override val name: String, val input: String) : SignalProvider {
-    override fun output(segments: SegmentMap): Int? =
-        if (input.isNumberLiteral()) input.toInt()
-        else knownSignals.signalTo(input) { id -> segments[id]?.output(segments) }
+private sealed interface SignalProvider {
+    val name: String
+    fun outputOf(segments: SegmentMap): Int?
 }
 
-private class Gate(override val name: String, val inputA: String, val inputB: String, val op: Op) : SignalProvider {
-    override fun output(segments: SegmentMap): Int? {
-        val calc: (String) -> Int? = { id -> segments[id]?.output(segments) }
-        val valueA = if (inputA.isNumberLiteral()) inputA.toInt() else knownSignals.signalTo(inputA, calc)
-        val valueB = if (inputB.isNumberLiteral()) inputB.toInt() else knownSignals.signalTo(inputB, calc)
+/** Extension function to check if value is a numeric literal */
+private fun String.isNumber(): Boolean = all { it.isDigit() }
+
+private class Value(override val name: String, val input: String) : SignalProvider {
+    override fun outputOf(segments: SegmentMap): Int? =
+        if (input.isNumber()) input.toInt()
+        else knownSignals.signalTo(input) { id -> segments[id]?.outputOf(segments) }
+}
+
+private class Gate(override val name: String, val op: Op, val inputA: String, val inputB: String = "*") :
+    SignalProvider {
+    override fun outputOf(segments: SegmentMap): Int? {
+        val calc: (String) -> Int? = { id -> segments[id]?.outputOf(segments) }
+        val valueA = if (inputA.isNumber()) inputA.toInt() else knownSignals.signalTo(inputA, calc)
+        val valueB = if (inputB.isNumber()) inputB.toInt() else knownSignals.signalTo(inputB, calc)
         return if (valueA == null || valueB == null) null else op.eval(valueA, valueB)
     }
 }
 
 private class Circuit(val segments: SegmentMap) {
 
-    fun signalTo(id: String): Int? = segments[id]?.output(segments)
+    fun signalTo(id: String): Int? = segments[id]?.outputOf(segments)
 
     companion object {
-        private const val NOT_USED = "_"
+        private const val NO_SIGNAL = "_"
+
         fun assemble(instructions: List<String>): Circuit {
             val segments = mutableMapOf<String, SignalProvider>()
-            segments[NOT_USED] = Value("0", "0")
+            segments[NO_SIGNAL] = Value("0", "0")
             instructions.forEach { instruction ->
-                val parts = instruction.split(" ")
-                val segment: SignalProvider = when (parts.size) {
-                    3 -> Value(name = parts.last(), parts.first())                   // ? -> wire
-                    4 -> Gate(name = parts.last(), parts[1], NOT_USED, Op.NOT)                  // NOT wire -> wire
-                    5 -> Gate(name = parts.last(), parts[0], parts[2], Op.valueOf(parts[1]))    // a OP b -> wire
-                    else -> throw UnsupportedOperationException("Unsupported instruction: $instruction")
-                }
+                val segment = signalProviderFrom(instruction)
                 segments[segment.name] = segment
             }
             return Circuit(segments)
+        }
+
+        private fun signalProviderFrom(instruction: String): SignalProvider {
+            val parts = instruction.split(" ")
+            return when (parts.size) {
+                3 -> Value(name = parts.last(), parts.first())                              // ? -> wire
+                4 -> Gate(name = parts.last(), Op.NOT, parts[1], NO_SIGNAL)                 // NOT wire -> wire
+                5 -> Gate(name = parts.last(), Op.valueOf(parts[1]), parts[0], parts[2])    // a OP b -> wire
+                else -> throw UnsupportedOperationException("Unsupported instruction: $instruction")
+            }
         }
     }
 }
