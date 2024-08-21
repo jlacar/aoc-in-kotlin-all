b0ed7bc342db242274fc4aa72c3d422483c2348d
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-08-21 11:30:21 -0400

    r - Convert parameter to receiver

--- src/main/kotlin/lacar/junilu/Day05.kt
+++ src/main/kotlin/lacar/junilu/Day05.kt
@@ -11,10 +11,10 @@ class Day05(private val strings: List<String>) {
 
     // --- PART 1 ---
 
-    fun countOfNiceStrings() = strings.count { isNice(it) }
+    fun countOfNiceStrings() = strings.count { it.isNice() }
 
-    private fun isNice(s: String) =
-        s.hasEnoughVowels() && s.hasDoubleLetters() && s.hasNoDisallowedSubstrings()
+    private fun String.isNice() =
+        hasEnoughVowels() && hasDoubleLetters() && hasNoDisallowedSubstrings()
 
     private fun String.hasNoDisallowedSubstrings() =
         windowed(2).none { listOf("ab", "cd", "pq", "xy").contains(it) }
@@ -27,9 +27,9 @@ class Day05(private val strings: List<String>) {
 
     // --- PART 2 ---
 
-    fun countOfNicerStrings() = strings.count { isNicer(it) }
+    fun countOfNicerStrings() = strings.count { it.isNicer() }
 
-    private fun isNicer(s: String) = s.hasNonOverlappingPair() && s.hasThreeLetterPalindrome()
+    private fun String.isNicer() = hasNonOverlappingPair() && hasThreeLetterPalindrome()
 
     private fun String.hasThreeLetterPalindrome() =
         windowed(3).any { it.first() == it.last() }
