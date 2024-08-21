1feeba78fe5adf84f1f9e2340e012c23bdcbfa14
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-08-21 12:00:45 -0400

    r - Tidy up: rename and simplify

--- src/main/kotlin/lacar/junilu/Day05.kt
+++ src/main/kotlin/lacar/junilu/Day05.kt
@@ -14,15 +14,15 @@ class Day05(private val strings: List<String>) {
     fun countOfNiceStrings() = strings.count { it.isNice() }
 
     private fun String.isNice() =
-        hasEnoughVowels() && hasDoubleLetters() && hasNoDisallowedSubstrings()
+        hasAtLeastThreeVowels() && hasDoubleLetters() && hasNoDisallowedSubstrings()
 
     private fun String.hasNoDisallowedSubstrings() =
-        windowed(2).none { listOf("ab", "cd", "pq", "xy").contains(it) }
+        listOf("ab", "cd", "pq", "xy").none { contains(it) }
 
     private fun String.hasDoubleLetters() =
         windowed(2).any { it.first() == it.last() }
 
-    private fun String.hasEnoughVowels(): Boolean =
+    private fun String.hasAtLeastThreeVowels(): Boolean =
         filter { "aeiou".contains(it) }.length >= 3
 
     // --- PART 2 ---
@@ -36,6 +36,6 @@ class Day05(private val strings: List<String>) {
 
     private fun String.hasNonOverlappingPair(): Boolean {
         val pairs = windowed(2)
-        return pairs.any { abs(pairs.indexOf(it) - pairs.lastIndexOf(it)) > 1 }
+        return pairs.any { abs(indexOf(it) - lastIndexOf(it)) > 1 }
     }
 }
