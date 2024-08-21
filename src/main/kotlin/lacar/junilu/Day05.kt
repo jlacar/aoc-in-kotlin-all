43f75d613fa5ec357c713cfc56cadb3f92885039
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-08-21 11:27:27 -0400

    r - Changed parameter to receiver

--- src/main/kotlin/lacar/junilu/Day05.kt
+++ src/main/kotlin/lacar/junilu/Day05.kt
@@ -14,28 +14,28 @@ class Day05(private val strings: List<String>) {
     fun countOfNiceStrings() = strings.count { isNice(it) }
 
     private fun isNice(s: String) =
-        hasEnoughVowels(s) && hasDoubleLetters(s) && hasNoDisallowedLetters(s)
+        s.hasEnoughVowels() && s.hasDoubleLetters() && s.hasNoDisallowedSubstrings()
 
-    private fun hasNoDisallowedLetters(s: String) =
-        s.windowed(2).none { listOf("ab", "cd", "pq", "xy").contains(it) }
+    private fun String.hasNoDisallowedSubstrings() =
+        windowed(2).none { listOf("ab", "cd", "pq", "xy").contains(it) }
 
-    private fun hasDoubleLetters(s: String) =
-        s.windowed(2).any { it.first() == it.last() }
+    private fun String.hasDoubleLetters() =
+        windowed(2).any { it.first() == it.last() }
 
-    private fun hasEnoughVowels(s: String): Boolean =
-        s.filter { "aeiou".contains(it) }.length >= 3
+    private fun String.hasEnoughVowels(): Boolean =
+        filter { "aeiou".contains(it) }.length >= 3
 
     // --- PART 2 ---
 
     fun countOfNicerStrings() = strings.count { isNicer(it) }
 
-    private fun isNicer(s: String) = hasNonOverlappingPair(s) && hasThreeLetterPalindrome(s)
+    private fun isNicer(s: String) = s.hasNonOverlappingPair() && s.hasThreeLetterPalindrome()
 
-    private fun hasThreeLetterPalindrome(s: String) =
-        s.windowed(3).any { it.first() == it.last() }
+    private fun String.hasThreeLetterPalindrome() =
+        windowed(3).any { it.first() == it.last() }
 
-    private fun hasNonOverlappingPair(s: String): Boolean {
-        val pairs = s.windowed(2)
+    private fun String.hasNonOverlappingPair(): Boolean {
+        val pairs = windowed(2)
         return pairs.any { abs(pairs.indexOf(it) - pairs.lastIndexOf(it)) > 1 }
     }
 }
