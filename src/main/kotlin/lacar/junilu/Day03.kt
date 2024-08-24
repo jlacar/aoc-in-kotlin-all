656e68b5f6d7a3b76a1a2a209eefc66705ad9be6
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-08-23 22:07:03 -0400

    r - Make HouseLocation a data class instead of typealias. Use sets instead of distinct(). Renamed a few things.

--- src/main/kotlin/lacar/junilu/Day03.kt
+++ src/main/kotlin/lacar/junilu/Day03.kt
@@ -1,6 +1,6 @@
 package lacar.junilu
 
-private typealias HouseLocation = Pair<Int, Int>
+private data class HouseLocation(val row: Int, val col: Int)
 
 /**
  * AoC 2015 - Day 3: Perfectly Spherical Houses in a Vacuum
@@ -8,10 +8,10 @@ private typealias HouseLocation = Pair<Int, Int>
  * https://adventofcode.com/2015/day/3
  */
 class Day03(private val directions: String) {
-    fun housesSantaVisited() = housesVisited(directions).distinct().count()
+    fun housesSantaVisited() = housesVisited(directions).count()
 
     fun housesSantaOrRobotSantaVisited() =
-        (housesVisited(bySanta) + housesVisited(byRobotSanta)).distinct().count()
+        (housesVisited(bySanta) union housesVisited(byRobotSanta)).count()
 
     private val evenIndicesOnly = { index: Int, _: Char -> index % 2 == 0 }
     private val oddIndicesOnly = { index: Int, _: Char -> index % 2 == 1 }
@@ -19,16 +19,16 @@ class Day03(private val directions: String) {
     private val bySanta = directions.filterIndexed(evenIndicesOnly)
     private val byRobotSanta = directions.filterIndexed(oddIndicesOnly)
 
-    private val startLocation = Pair(0, 0)
+    private val startLocation = HouseLocation(0, 0)
 
-    private fun housesVisited(directions: String): List<HouseLocation> =
+    private fun housesVisited(directions: String): Set<HouseLocation> =
         directions.runningFold(startLocation) { currentLocation, direction ->
-            val (horizontal, vertical) = currentLocation
+            val (row, col) = currentLocation
             when (direction) {
-                '<' -> currentLocation.copy(first = horizontal - 1)
-                '>' -> currentLocation.copy(first = horizontal + 1)
-                '^' -> currentLocation.copy(second = vertical + 1)
-                else -> currentLocation.copy(second = vertical - 1)
+                '<' -> currentLocation.copy(col = col - 1)
+                '>' -> currentLocation.copy(col = col + 1)
+                '^' -> currentLocation.copy(row = row + 1)
+                else -> currentLocation.copy(row = row - 1)
             }
-        }
+        }.toSet()
 }
