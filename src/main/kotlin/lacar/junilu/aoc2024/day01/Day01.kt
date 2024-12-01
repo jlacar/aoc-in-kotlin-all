7fd8982dce9aa4efe22154e71c644e5921262433
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-01 10:23:52 -0500

    r!! - Replace Math.abs() with Kotlin abs() per IDE suggestion

--- src/main/kotlin/lacar/junilu/aoc2024/day01/Day01.kt
+++ src/main/kotlin/lacar/junilu/aoc2024/day01/Day01.kt
@@ -1,11 +1,13 @@
 package lacar.junilu.aoc2024.day01
 
+import kotlin.math.abs
+
 /**
  * AoC 2024 - Day 1: Historian Hysteria
  */
 object Day01 {
     fun totalDistance(column1: List<Int>, column2: List<Int>) =
-        (column1.indices).map { i -> Math.abs(column1[i] - column2[i]) }.sum()
+        (column1.indices).map { i -> abs(column1[i] - column2[i]) }.sum()
 
     fun similarityScore(column1: List<Int>, column2: List<Int>): Int =
         column1.map { n -> column2.count { it == n } * n }.sum()
