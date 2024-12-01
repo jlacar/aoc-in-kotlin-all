4d99468524136aa6dfc0944599aeaa87724f9083
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-01 10:43:20 -0500

    r!! - Minor tidying - add explicit return type to function expression

--- src/main/kotlin/lacar/junilu/aoc2024/day01/Day01.kt
+++ src/main/kotlin/lacar/junilu/aoc2024/day01/Day01.kt
@@ -6,7 +6,7 @@ import kotlin.math.abs
  * AoC 2024 - Day 1: Historian Hysteria
  */
 object Day01 {
-    fun totalDistance(column1: List<Int>, column2: List<Int>) =
+    fun totalDistance(column1: List<Int>, column2: List<Int>): Int =
         (column1.indices).map { i -> abs(column1[i] - column2[i]) }.sum()
 
     fun similarityScore(column1: List<Int>, column2: List<Int>): Int =
