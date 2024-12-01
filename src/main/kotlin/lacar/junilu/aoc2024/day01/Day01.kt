0fa03528edcb799e902ac678c1ce0e3294a8f31b
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-01 00:45:30 -0500

    r - Minor tidying, moved functions into object Day01

--- src/main/kotlin/lacar/junilu/aoc2024/day01/Day01.kt
+++ src/main/kotlin/lacar/junilu/aoc2024/day01/Day01.kt
@@ -1,31 +1,50 @@
 package lacar.junilu.aoc2024.day01
 
-import lacar.junilu.println
+import lacar.junilu.*
 
 /**
  * AoC 2024 - Day 1: Historian Hysteria
  */
 
-fun parse(input: List<String>): Pair<List<Int>, List<Int>> {
-    val list1 = mutableListOf<Int>()
-    val list2 = mutableListOf<Int>()
-    input.map { line ->
-        val (n1, n2) = line.split("   ").map { it.toInt() }
-        list1.add(n1)
-        list2.add(n2)
+object Day01 {
+    val puzzleInput = readPuzzleInput("aoc2024/day01-gh")
+
+    fun part1(): Int {
+        val (list1, list2) = parse(puzzleInput)
+
+        val sorted1 = list1.sorted()
+        val sorted2 = list2.sorted()
+
+        return sorted1.mapIndexed { i, n -> Math.abs(n - sorted2[i]) }.sum()
     }
-    return Pair(list1, list2)
+
+    fun part2(): Int {
+        val (list1, list2) = parse(puzzleInput)
+
+        return list1.map { n -> list2.count { it == n } * n }.sum()
+    }
+
+    fun parse(input: List<String>): Pair<List<Int>, List<Int>> {
+        val list1 = mutableListOf<Int>()
+        val list2 = mutableListOf<Int>()
+        input.map { line ->
+            val (n1, n2) = line.split("   ")
+            list1.add(n1.toInt())
+            list2.add(n2.toInt())
+        }
+        return Pair(list1, list2)
+    }
 }
 
+
 fun main() {
-    val (list1, list2) = parse(lacar.junilu.readPuzzleInput("aoc2024/day01-gh"))
+    val totalDistance = Day01.part1()
+    println("Part 1: $totalDistance")
+    check(totalDistance == 2285373, lazyMessage =  { "FAILED" })
 
-    val sorted1 = list1.sorted()
-    val sorted2 = list2.sorted()
-
-    sorted1.mapIndexed { i, n -> Math.abs(n - sorted2[i]) }.sum().println()
-
-    sorted1.map { n -> sorted2.count { it == n } * n }.sum().println()
+    val similarityScore = Day01.part2()
+    println("Part 2: $similarityScore")
+    check(similarityScore == 21142653, lazyMessage =  { "FAILED" })
 }
 
 /*
