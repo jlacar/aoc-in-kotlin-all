a20b33050d8f23884fbe753b873f3945e5198090
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-01 09:09:32 -0500

    f** - Get files ready for 2024 Day 2

---
+++ src/main/kotlin/lacar/junilu/aoc2024/day02/Day02.kt
@@ -0,0 +1,10 @@
+package lacar.junilu.aoc2024.day02
+
+/**
+ * AoC 2024: Day 2 -
+ */
+object Day02 {
+    fun part1() = 0
+
+    fun part2() = 0
+}
---
+++ src/test/kotlin/lacar/junilu/aoc2024/day02/Day02Test.kt
@@ -0,0 +1,26 @@
+package lacar.junilu.aoc2024.day02
+
+import lacar.junilu.readPuzzleInput
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day02Test {
+    @Test
+    fun `Solution - Part 1`() {
+        assertEquals(-1, Day02.part1())
+    }
+
+    @Test
+    fun `Solution - Part 2`() {
+        assertEquals(-1, Day02.part2())
+    }
+
+    companion object {
+//        private val puzzleInputGitHub = readPuzzleInput("aoc2024/day02-gh")
+
+        @JvmStatic
+        fun parse() {
+
+        }
+    }
+}
