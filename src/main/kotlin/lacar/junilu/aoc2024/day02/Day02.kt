368e38a2d27c7a8632ffd29e4845e5eccccac7d0
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-02 16:44:54 -0500

    r!! - Inline `zipWithNext()` transform function and renamed to `levelChanges`.
    Also, add typed parameter `change` to the predicates and let Kotlin infer the type for `decreasing`, `increasing`, and `inSafeRange`.

--- src/main/kotlin/lacar/junilu/aoc2024/day02/Day02.kt
+++ src/main/kotlin/lacar/junilu/aoc2024/day02/Day02.kt
@@ -13,13 +13,12 @@ object Day02 {
         }
 
     private fun List<Int>.isSafe(): Boolean {
-        val changeInLevels: (Int, Int) -> Int = { thisLevel, nextLevel -> nextLevel - thisLevel }
-        val decreasing: (Int) -> Boolean = { it < 0 }
-        val increasing: (Int) -> Boolean = { it > 0 }
-        val inSafeRange: (Int) -> Boolean = { abs(it) in 1..3 }
+        val levelChanges = zipWithNext { thisLevel, nextLevel -> nextLevel - thisLevel }
+        val decreasing = { change: Int -> change < 0 }
+        val increasing = { change: Int -> change > 0 }
+        val inSafeRange = { change: Int -> abs(change) in 1..3 }
 
-        val changes = zipWithNext(changeInLevels)
-        return changes.all(inSafeRange) && (changes.all(decreasing) || changes.all(increasing))
+        return levelChanges.all(inSafeRange) && (levelChanges.all(decreasing) || levelChanges.all(increasing))
     }
 
     private fun List<Int>.canBeDampened() =
