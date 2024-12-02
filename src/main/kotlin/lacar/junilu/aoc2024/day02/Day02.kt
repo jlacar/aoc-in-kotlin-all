34a26ae23f927a5729497d9bd0d391ebf0d80acb
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-02 02:09:15 -0500

    SR++ - Tidied up structure and organization, with lots of renaming.

--- src/main/kotlin/lacar/junilu/aoc2024/day02/Day02.kt
+++ src/main/kotlin/lacar/junilu/aoc2024/day02/Day02.kt
@@ -1,49 +1,28 @@
 package lacar.junilu.aoc2024.day02
 
-import lacar.junilu.aoc2024.day02.Day02.reports
-import lacar.junilu.println
-import lacar.junilu.readPuzzleInput
 import kotlin.math.abs
 
 /**
  * AoC 2024: Day 2 -
  */
 object Day02 {
-    val reports = readPuzzleInput("aoc2024/day02-gh").map { line ->
-        line.split(" ").map(String::toInt)
+    fun howManyAreSafe(reports: List<List<Int>>) =
+        reports.count { it.isSafe() }
+
+    fun howManyAreSafeWithDampener(reports: List<List<Int>>) =
+        reports.count { it.isSafe() || it.canBeDampened() }
+
+    private fun List<Int>.isSafe(): Boolean {
+        val changes = windowed(2).map { pair -> pair.first() - pair.last() }
+        return changes.areInSafeRange() && changes.areConsistent()
     }
 
-    fun part1(reports: List<List<Int>>) = reports.count { isSafe(it) }
-//        .also { it.println() } }
+    private fun List<Int>.areInSafeRange() =
+        all { abs(it) in 1..3 }
 
-    fun isSafe(report: List<Int>): Boolean {
-        val diffs = report.windowed(2).map { pair -> pair.first() - pair.last() }
-        return diffs.none { it == 0 } && diffs.all { abs(it) in 1..3 } && (diffs.all { it < 0 } || diffs.all { it > 0 })
-    }
+    private fun List<Int>.areConsistent() =
+        all { it < 0 } || all { it > 0 }
 
-    fun part2(reports: List<List<Int>>): Int = reports.count { isSafe(it) || canBeMadeSafe(it) }
-
-    fun canBeMadeSafe(report: List<Int>): Boolean {
-        return report.indices.map { (report.take(it) + report.drop(it + 1)) }.any { isSafe(it) }
-    }
-}
-
-fun main() {
-
-    val reports = """
-        7 6 4 2 1
-        1 2 7 8 9
-        9 7 6 2 1
-        1 3 2 4 5
-        8 6 4 4 1
-        1 3 6 7 9
-    """.trimIndent().lines().map { line -> line.split(" ").map(String::toInt) }
-
-//    Day02.canBeMadeSafe(listOf(1, 2, 7, 8, 9)).println()
-
-//    Day02.part1(reports).println()
-//    Day02.part2(reports).println()
-
-//    Day02.part1(Day02.reports).println()
-    Day02.part2(Day02.reports).println()
+    private fun List<Int>.canBeDampened() =
+        indices.map { (take(it) + drop(it + 1)) }.any { it.isSafe() }
 }
--- src/test/kotlin/lacar/junilu/aoc2024/day02/Day02Test.kt
+++ src/test/kotlin/lacar/junilu/aoc2024/day02/Day02Test.kt
@@ -7,20 +7,42 @@ import org.junit.jupiter.api.Test
 class Day02Test {
     @Test
     fun `Solution - Part 1`() {
-        assertEquals(-1, Day02.part1())
+        assertEquals(516, Day02.howManyAreSafe(puzzleInputGitHub))
+        assertEquals(282, Day02.howManyAreSafe(puzzleInputGmail))
     }
 
     @Test
     fun `Solution - Part 2`() {
-        assertEquals(-1, Day02.part2())
+        assertEquals(561, Day02.howManyAreSafeWithDampener(puzzleInputGitHub))
+        assertEquals(349, Day02.howManyAreSafeWithDampener(puzzleInputGmail))
+    }
+
+    private val exampleReports = parse("""
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
+    """.trimIndent().lines())
+
+    @Test
+    fun `Example - Part 1`() {
+        assertEquals(2, Day02.howManyAreSafe(exampleReports))
+    }
+
+    @Test
+    fun `Example - Part 2`() {
+        assertEquals(4, Day02.howManyAreSafeWithDampener(exampleReports))
     }
 
     companion object {
-//        private val puzzleInputGitHub = readPuzzleInput("aoc2024/day02-gh")
+        private val puzzleInputGitHub = parse(readPuzzleInput("aoc2024/day02-gh"))
+        private val puzzleInputGmail = parse(readPuzzleInput("aoc2024/day02-gm"))
 
         @JvmStatic
-        fun parse() {
-
+        fun parse(input: List<String>) = input.map { line ->
+            line.split(" ").map(String::toInt)
         }
     }
 }
