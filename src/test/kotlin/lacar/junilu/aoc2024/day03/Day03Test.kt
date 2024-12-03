157cdbad0a87ee3d7f864c38e7e3c439c8755312
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-03 03:17:31 -0500

    r++ - Refactor Example for 2024 Day 3 Part 1

--- src/test/kotlin/lacar/junilu/aoc2024/day03/Day03Test.kt
+++ src/test/kotlin/lacar/junilu/aoc2024/day03/Day03Test.kt
@@ -20,8 +20,7 @@ class Day03Test {
 
     @Test
     fun `Example - Part 1`() {
-        val matches = parse("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
-        assertEquals(161, matches.sumOf { (n1, n2) -> n1 * n2 })
+        assertEquals(161, Day03.part1("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"))
     }
 
     @Test
