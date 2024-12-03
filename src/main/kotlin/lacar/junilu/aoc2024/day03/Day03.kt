06a1ba6862006e27c5a63709590c8979c8df8593
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-03 11:09:36 -0500

    r!! - Refactor both parts of 2024 Day 3 to handle `List<String>` input.
    Generalized the solutions to handle `List<String>` instead of just a single `String` as input. Alternatively, could have just used `joinToString()` to convert a `List<String>` to a single String but that would probably run into memory limits for input with many lines.

--- src/main/kotlin/lacar/junilu/aoc2024/day03/Day03.kt
+++ src/main/kotlin/lacar/junilu/aoc2024/day03/Day03.kt
@@ -4,11 +4,14 @@ package lacar.junilu.aoc2024.day03
  * AoC 2024 - Day 3: Mull It Over
  */
 object Day03 {
-    fun part1(input: String): Int =
+
+    fun part1(input: List<String>): Int = input.sumOf { line -> part1Sum(line) }
+
+    private fun part1Sum(line: String): Int =
         """mul\((\d+,\d+)\)""".toRegex()
-        .findAll(input)
-        .map { it.groupValues[1].toPair() }
-        .sumOf { (n1, n2) -> n1 * n2 }
+            .findAll(line)
+            .map { it.groupValues[1].toPair() }
+            .sumOf { (n1, n2) -> n1 * n2 }
 
     /**
      * Convert a string of the form "digits,digits" to a Pair<Int, Int>.
@@ -18,12 +21,19 @@ object Day03 {
         return Pair(n1, n2)
     }
 
-    fun part2(input: String): Int {
+    fun part2(input: List<String>): Int =
+        input.fold(Pair(0, true)) { acc, line ->
+            val (runningSum, enabled) = acc
+            val (currentSum, nextEnabled) = part2Sum(line, enabled)
+            Pair(runningSum + currentSum, nextEnabled)
+        }.first
+
+    private fun part2Sum(input: String, startEnabled: Boolean = true): Pair<Int, Boolean> {
         val operands = mutableListOf<Pair<Int, Int>>()
-        """mul\((\d+,\d+)\)|(don't\(\))|(do\(\))""".toRegex()
+        val endEnabled = """mul\((\d+,\d+)\)|(don't\(\))|(do\(\))""".toRegex()
             .findAll(input)
-            .fold(true) { enabled, match -> addTo(operands, match, enabled) }
-        return operands.sumOf { (n1, n2) -> n1 * n2 }
+            .fold(startEnabled) { enabled, match -> addTo(operands, match, enabled) }
+        return Pair(operands.sumOf { (n1, n2) -> n1 * n2 }, endEnabled)
     }
 
     private fun addTo(
--- src/test/kotlin/lacar/junilu/aoc2024/day03/Day03Test.kt
+++ src/test/kotlin/lacar/junilu/aoc2024/day03/Day03Test.kt
@@ -17,15 +17,15 @@ class Day03Test {
 
     @Test
     fun `Example - Part 1`() {
-        assertEquals(161, Day03.part1("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"))
+        assertEquals(161, Day03.part1(listOf("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")))
     }
 
     @Test
     fun `Example - Part 2`() {
-        assertEquals(48, Day03.part2("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"))
+        assertEquals(48, Day03.part2(listOf("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")))
     }
 
     companion object {
-        val puzzleInputGitHub = readPuzzleInput("aoc2024/day03-gh").joinToString("")
+        val puzzleInputGitHub = readPuzzleInput("aoc2024/day03-gh")
     }
 }
