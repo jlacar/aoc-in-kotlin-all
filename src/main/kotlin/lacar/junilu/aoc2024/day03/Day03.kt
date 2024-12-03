bdbcf528778b87282f42fc93e2f9f903c43b4a44
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-03 07:23:44 -0500

    r!! - Tighten up call chains.

--- src/main/kotlin/lacar/junilu/aoc2024/day03/Day03.kt
+++ src/main/kotlin/lacar/junilu/aoc2024/day03/Day03.kt
@@ -4,15 +4,12 @@ package lacar.junilu.aoc2024.day03
  * AoC 2024 - Day 3: Mull It Over
  */
 object Day03 {
-    fun part1(input: String): Int {
-        val regex = """mul\((\d+,\d+)\)""".toRegex()
-        val matches = regex.findAll(input)
-
-        val ops = matches.map { it.groupValues[1] }
-        val pairs = ops.map { it.toPair() }
-
-        return pairs.sumOf { (n1, n2) -> n1 * n2 }
-    }
+    fun part1(input: String): Int =
+        """mul\((\d+,\d+)\)""".toRegex()
+        .findAll(input)
+        .map { it.groupValues[1] }      // (ds,ds)
+        .map { it.toPair() }            // Pair<Int, Int>
+        .sumOf { (n1, n2) -> n1 * n2 }
 
     private fun String.toPair(): Pair<Int, Int> {
         val (n1, n2) = split(",").map(String::toInt)
@@ -20,21 +17,16 @@ object Day03 {
     }
 
     fun part2(input: String): Int {
-        val regex = """mul\((\d+,\d+)\)|(don't\(\))|(do\(\))""".toRegex()
-        val matches = regex.findAll(input)
-
+        val zero = Pair(0, 0)
         var emit = true
-        return matches.map { match ->
-            val zero = Pair(0, 0)
-            when (match.groupValues[0]) {
-                "do()" -> zero.also { emit = true }
-                "don't()" -> zero.also { emit = false }
-                else -> if (emit) {
-                    match.groupValues[1].toPair()
-                } else {
-                    zero
+        return """mul\((\d+,\d+)\)|(don't\(\))|(do\(\))""".toRegex()
+            .findAll(input)
+            .map {
+                when (it.groupValues[0]) {
+                    "do()" -> zero.also { emit = true }
+                    "don't()" -> zero.also { emit = false }
+                    else -> if (emit) it.groupValues[1].toPair() else zero
                 }
-            }
-        }.sumOf { (n1, n2) -> n1 * n2 }
+            }.sumOf { (n1, n2) -> n1 * n2 }
     }
 }
