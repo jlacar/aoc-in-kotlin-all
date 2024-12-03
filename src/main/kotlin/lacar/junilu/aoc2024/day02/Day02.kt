794a142f567549e90dc563e08d494154b447ef21
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-03 15:27:21 -0500

    Revert "r!! - Trying Todd Ginsberg approach to expressing safety."
    This reverts commit dd92fd09900925632c31a7730bb30b29a34735f0.

--- src/main/kotlin/lacar/junilu/aoc2024/day02/Day02.kt
+++ src/main/kotlin/lacar/junilu/aoc2024/day02/Day02.kt
@@ -1,5 +1,7 @@
 package lacar.junilu.aoc2024.day02
 
+import kotlin.math.abs
+
 /**
  * AoC 2024 - Day 2: Red-Nosed Reports
  */
@@ -12,10 +14,11 @@ object Day02 {
 
     private fun List<Int>.isSafe(): Boolean {
         val levelChanges = zipWithNext { thisLevel, nextLevel -> nextLevel - thisLevel }
-        val safelyDecreasing = { change: Int -> change in -3..-1 }
-        val safelyIncreasing = { change: Int -> change in 1..3 }
+        val decreasing = { change: Int -> change < 0 }
+        val increasing = { change: Int -> change > 0 }
+        val inSafeRange = { change: Int -> abs(change) in 1..3 }
 
-        return levelChanges.all(safelyDecreasing) || levelChanges.all(safelyIncreasing)
+        return levelChanges.all(inSafeRange) && (levelChanges.all(decreasing) || levelChanges.all(increasing))
     }
 
     private fun List<Int>.canBeDampened() =
