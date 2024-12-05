ba70b042b6e9a4d6aedc6d27c787a03b7716f6b2
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-05 00:14:26 -0500

    f** - Parse input - separate first and second sections.

--- src/main/kotlin/lacar/junilu/aoc2024/day05/Day05.kt
+++ src/main/kotlin/lacar/junilu/aoc2024/day05/Day05.kt
@@ -1,6 +1,7 @@
 package lacar.junilu.aoc2024.day05
 
 import lacar.junilu.experimental.solution
+import lacar.junilu.println
 
 /**
  * AoC 2024 - Day 5: Print Queue
@@ -8,11 +9,20 @@ import lacar.junilu.experimental.solution
  * https://adventofcode.com/2024/day/5
  */
 object Day05 {
-    val part1 get() = solution {
-        TODO("Not implemented")
+    val part1 get() = solution { input ->
+        val (rules, pages) = parse(input)
+
+        // What do you get if you add up the middle page number from those correctly-ordered updates?
+        -1
     }
 
     val part2 get() = solution {
         TODO("Not implemented")
+    }
+
+    private fun parse(input: List<String>): Pair<Int, Int> {
+        val (first, second) = input.joinToString("\n").split("\n\n")
+        "First section: $first\nSecond section: $second".println()
+        return Pair(0, 0)
     }
 }
