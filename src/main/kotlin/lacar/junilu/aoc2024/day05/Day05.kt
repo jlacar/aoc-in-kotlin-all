38c0a56ba093c0bbc025c401100551950ff9b785
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-05 00:03:33 -0500

    t** - Added example input to test

--- src/main/kotlin/lacar/junilu/aoc2024/day05/Day05.kt
+++ src/main/kotlin/lacar/junilu/aoc2024/day05/Day05.kt
@@ -2,6 +2,11 @@ package lacar.junilu.aoc2024.day05
 
 import lacar.junilu.experimental.solution
 
+/**
+ * AoC 2024 - Day 5: Print Queue
+ *
+ * https://adventofcode.com/2024/day/5
+ */
 object Day05 {
     val part1 get() = solution {
         TODO("Not implemented")
