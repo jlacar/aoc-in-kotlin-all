0bb0df962393c2af876f081638b447c8af605284
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-04 23:02:53 -0500

    R!! - Rework 2024 Day 1 to use Puzzle DSL.
    Part of experiment with an AoC Puzzle DSL. Inspired by Marcin Wisnioski's (@Nohus) work.

--- src/main/kotlin/lacar/junilu/aoc2024/day01/Day01.kt
+++ src/main/kotlin/lacar/junilu/aoc2024/day01/Day01.kt
@@ -1,18 +1,37 @@
 package lacar.junilu.aoc2024.day01
 
+import lacar.junilu.experimental.solution
 import kotlin.math.abs
 
 /**
  * AoC 2024 - Day 1: Historian Hysteria
  */
 object Day01 {
-    fun totalDistance(left: List<Int>, right: List<Int>): Int {
+
+    /* What is the total distance between your lists? */
+    val part1 get() = solution { input ->
+        val (left, right) = toColumns(input)
+
+        // answer:
         val leftSorted = left.sorted()
         val rightSorted = right.sorted()
-
-        return leftSorted.indices.sumOf { i -> abs(leftSorted[i] - rightSorted[i]) }
+        leftSorted.indices.sumOf { i -> abs(leftSorted[i] - rightSorted[i]) }
     }
 
-    fun similarityScore(left: List<Int>, right: List<Int>): Int =
+    /* What is the total similarity score? */
+    val part2 get() = solution { input ->
+        val (left, right) = toColumns(input)
+
+        // answer:
         left.sumOf { n -> right.count { it == n } * n }
+    }
+
+    // parse Input
+    private fun toColumns(input: List<String>): Pair<List<Int>, List<Int>> {
+        val (left, right) = input.map { it.split("   ") }
+            .map { (left, right) -> left.toInt() to right.toInt() }
+            .unzip()
+
+        return Pair(left, right)
+    }
 }
--- src/test/kotlin/lacar/junilu/aoc2024/day01/Day01Test.kt
+++ src/test/kotlin/lacar/junilu/aoc2024/day01/Day01Test.kt
@@ -1,29 +1,51 @@
 package lacar.junilu.aoc2024.day01
 
+import lacar.junilu.experimental.Puzzle
 import lacar.junilu.readPuzzleInput
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import kotlin.math.abs
 
 class Day01Test {
+
+    @Test
+    fun `Solution - Part 1 using Puzzle DSL`() {
+        assertEquals(2285373, usingInputForGithub.solve(Day01.part1))
+        assertEquals(2031679, usingInputForGmail.solve(Day01.part1))
+    }
+
+    @Test
+    fun `Solution - Part 2 using Puzzle DSL`() {
+        assertEquals(2285373, usingInputForGithub.solve(Day01.part2))
+        assertEquals(2031679, usingInputForGmail.solve(Day01.part2))
+    }
+
     @Test
     fun `Solutions - Part 1`() {
         val (leftGh, rightGh) = splitByColumn(puzzleInputGitHub)
-        assertEquals(2285373, Day01.totalDistance(leftGh, rightGh))
+        val leftSorted = leftGh.sorted()
+        val rightSorted = rightGh.sorted()
+        assertEquals(2285373, leftSorted.indices.sumOf { i -> abs(leftSorted[i] - rightSorted[i]) })
 
         val (leftGm, rightGm) = splitByColumn(puzzleInputGmail)
-        assertEquals(2031679, Day01.totalDistance(leftGm, rightGm))
+        val leftSorted1 = leftGm.sorted()
+        val rightSorted1 = rightGm.sorted()
+        assertEquals(2031679, leftSorted1.indices.sumOf { i -> abs(leftSorted1[i] - rightSorted1[i]) })
     }
 
     @Test
     fun `Solutions - Part 2`() {
         val (leftGh, rightGh) = splitByColumn(puzzleInputGitHub)
-        assertEquals(21142653, Day01.similarityScore(leftGh, rightGh))
+        assertEquals(21142653, leftGh.sumOf { n -> rightGh.count { it == n } * n })
 
         val (leftGm, rightGm) = splitByColumn(puzzleInputGmail)
-        assertEquals(19678534, Day01.similarityScore(leftGm, rightGm))
+        assertEquals(19678534, leftGm.sumOf { n -> rightGm.count { it == n } * n })
     }
 
     companion object {
+        private val usingInputForGithub = Puzzle.using("aoc2024/day01-gh")
+        private val usingInputForGmail = Puzzle.using("aoc2024/day01-gm")
+
         private val puzzleInputGitHub = readPuzzleInput("aoc2024/day01-gh")
         private val puzzleInputGmail = readPuzzleInput("aoc2024/day01-gm")
 
