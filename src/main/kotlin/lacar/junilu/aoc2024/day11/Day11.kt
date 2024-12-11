1155f144c78f65b4f32da5c3def6b2fd1c01c977
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-11 01:59:40 -0500

    F++ - 2024 Day 11 Part 1 SOLVED!

--- src/main/kotlin/lacar/junilu/aoc2024/day11/Day11.kt
+++ src/main/kotlin/lacar/junilu/aoc2024/day11/Day11.kt
@@ -1,12 +1,33 @@
 package lacar.junilu.aoc2024.day11
 
+import lacar.junilu.println
+
 class Day11(val stones: List<Long>) {
 
-    fun part1(blinks: Int): List<Long> =
+    fun part1(blinks: Int): List<Long> {
+        var lineOfStones = buildList { addAll(stones) }
 
-//    buildList {
-//        addAll("1 2024 1 0 9 9 2021976".split(" ").map { it.toInt() })
-//    }.map { it.toLong() }
+        repeat(blinks) { blink ->
+//            "$blink: $lineOfStones".println()
+            lineOfStones = lineOfStones.map {
+                when {
+                    it == 0L -> listOf(1L)
+                    it.hasEvenDigits() -> it.splitInTwo()
+                    else -> listOf(it * 2024)
+                }
+            }.flatten()
+        }
+//        "$blinks: $lineOfStones".println()
+        return lineOfStones
+    }
+
+    private fun Long.hasEvenDigits() = this.toString().length % 2 == 0
+
+    private fun Long.splitInTwo(): List<Long> {
+        val asString = this.toString()
+        val half = asString.length / 2
+        return listOf(asString.take(half).toLong(), asString.takeLast(half).toLong())
+    }
 
     companion object {
         fun using(line: String) = Day11(
--- src/test/kotlin/lacar/junilu/aoc2024/day11/Day11Test.kt
+++ src/test/kotlin/lacar/junilu/aoc2024/day11/Day11Test.kt
@@ -1,6 +1,9 @@
 package lacar.junilu.aoc2024.day11
 
+import lacar.junilu.println
+import lacar.junilu.readPuzzleInput
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -9,16 +12,29 @@ class Day11Test {
     @CsvSource(
         useHeadersInDisplayName = true,
         textBlock = """
-        stones,        times, count, first5
-        125 7,         5,    13,     1036288 7 2 20 24
-        125 7,         6,    22,     2097446912 14168 4048 2 0
-        0 1 10 99 999, 1,     7,     1 2024 1 0 9"""
+            stones,        times, count, first5
+            0 1 10 99 999, 1,      7,    1 2024 1 0 9
+            125 17,         5,     13,    1036288 7 2 20 24
+            125 17,         6,     22,    2097446912 14168 4048 2 0"""
     )
     fun `Example 1 - Part 1`(stones: String, times: Int, count: Int, first5: String) {
         val puzzle = Day11.using(stones)
         val listOfStones = puzzle.part1(times)
+        listOfStones.println()
+        assertAll(
+            { assertEquals(count, listOfStones.size) },
+            { assertEquals(first5, listOfStones.take(5).joinToString(" ")) }
+        )
+    }
 
-        assertEquals(count, listOfStones.size)
-        assertEquals(first5, listOfStones.take(5).joinToString(" "))
+    @Test
+    fun `Solution - Part 1`() {
+        assertEquals(218956, puzzleUsingGmailInput.part1(25).size)
+        assertEquals(186175, puzzleUsingGitHubInput.part1(25).size)
+    }
+
+    companion object {
+        val puzzleUsingGitHubInput = Day11.using(readPuzzleInput("aoc2024/day11-gh").first())
+        val puzzleUsingGmailInput = Day11.using(readPuzzleInput("aoc2024/day11-gm").first())
     }
 }
