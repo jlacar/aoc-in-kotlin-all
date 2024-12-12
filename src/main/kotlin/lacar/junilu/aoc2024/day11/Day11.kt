f43b7bb4a0760b0c6fd83f3158c937c835b1783f
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-11 22:16:57 -0500

    F - Adapt memoization pattern to a singleton Memoize object.
    Couldn't get Iliyan's Memo2 interface implementation to work with 75 blinks. Works fine with 25 blinks though. Either I did something wrong or there's still a problem with Iliyan's Memo classes.

--- src/main/kotlin/lacar/junilu/aoc2024/day11/Day11.kt
+++ src/main/kotlin/lacar/junilu/aoc2024/day11/Day11.kt
@@ -1,12 +1,30 @@
 package lacar.junilu.aoc2024.day11
 
 class Day11(private val stones: List<Long>) {
+    fun part1(blinks: Int): Long = stones.map { number -> Memoized.stoneCount(number, blinks) }.sum()
 
-    fun part1(blinks: Int): Long = stones.map { n -> howMany(n, blinks) }.sum()
+    companion object {
+        fun using(line: String) = Day11(
+            line.split(" ").map { it.toLong() }
+        )
+    }
+}
 
-//        (1..blinks).fold(stones.asSequence()) { acc: Sequence<Long>, _ ->
-//            acc.flatMap { numberOnStone -> next(numberOnStone) }
-//        }.sumOf { it }
+private object Memoized {
+    private val memo = mutableMapOf<Pair<Long, Int>, Long>()
+    fun stoneCount(stone: Long, blinks: Int): Long = memo.getOrPut(Pair(stone, blinks)) {
+        when (blinks) {
+            0 -> 1L
+            else -> when {
+                stone == 0L -> stoneCount(1L, blinks - 1)
+                stone.hasEvenDigits() -> {
+                    val (left, right) = stone.splitInTwo()
+                    stoneCount(left, blinks - 1) + stoneCount(right, blinks - 1)
+                }
+                else -> stoneCount(stone * 2024L, blinks - 1)
+            }
+        }
+    }
 
     private fun Long.hasEvenDigits() = this.toString().length % 2 == 0
 
@@ -14,27 +32,5 @@ class Day11(private val stones: List<Long>) {
         val thisAsString = this.toString()
         val half = thisAsString.length / 2
         return Pair(thisAsString.take(half).toLong(), thisAsString.takeLast(half).toLong())
-    }
-
-    private val memo = mutableMapOf<Pair<Long, Int>, Long>()
-
-    private fun howMany(stone: Long, blinks: Int): Long = memo.getOrPut(Pair(stone, blinks)) {
-        when (blinks) {
-            0 -> 1L
-            else -> when {
-                stone == 0L -> howMany(1L, blinks - 1)
-                stone.hasEvenDigits() -> {
-                    val (left, right) = stone.splitInTwo()
-                    howMany(left, blinks - 1) + howMany(right, blinks - 1)
-                }
-                else -> howMany(stone * 2024L, blinks - 1)
-            }
-        }
-    }
-
-    companion object {
-        fun using(line: String) = Day11(
-            line.split(" ").map { it.toLong() }
-        )
     }
 }
