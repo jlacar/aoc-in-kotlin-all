6032319abd0aa81eeaaeb665c4d4234839b846f8
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-12 03:32:22 -0500

    t** - RED test for Grid.getRegions().
    Doesn't account for non-contiguous regions.

--- src/test/kotlin/lacar/junilu/common/RegionTest.kt
+++ src/test/kotlin/lacar/junilu/common/RegionTest.kt
@@ -15,11 +15,30 @@ class RegionTest {
             EEEC
             """.trimIndent().lines()
         )
+
+        @JvmStatic
+        private val fiveRegionTwoSymbolGrid = Grid.parse(
+            """
+            OOOOO
+            OXOXO
+            OOOOO
+            OXOXO
+            OOOOO
+            """.trimIndent().lines()
+        )
     }
 
     @Test
-    fun `countRegions gives correct number of regions`() {
-        val regions = smallFiveGrid.getRegions()
-        assertEquals(5, regions.size)
+    fun `getRegions gives correct number of regions`() {
+        val smallFiveRegions = smallFiveGrid.getRegions()
+        assertEquals(5, smallFiveRegions.size)
+
+        val fiveRegionTwoSymbolRegions = fiveRegionTwoSymbolGrid.getRegions()
+        assertEquals(5, fiveRegionTwoSymbolRegions.size)
     }
+
+//    @Test
+//    fun `regions are contiguous groups with same symbol`() {
+//
+//    }
 }
