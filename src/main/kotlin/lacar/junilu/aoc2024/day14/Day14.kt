3a3b953bb58acea7153c382c7a71049ab1829091
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-17 03:10:43 -0500

    r!! - Continue tidying up 2024 Day 14

--- src/main/kotlin/lacar/junilu/aoc2024/day14/Day14.kt
+++ src/main/kotlin/lacar/junilu/aoc2024/day14/Day14.kt
@@ -1,6 +1,7 @@
 package lacar.junilu.aoc2024.day14
 
 import lacar.junilu.common.Point
+import lacar.junilu.common.wrap
 
 // region ===== Puzzle's Ubiquitous Language
 
@@ -32,15 +33,11 @@ class Day14(private var robots: List<Robot>) {
 
     fun part1(): Int = quadrants()
         .map { q -> robots
-            .move(100)
+            .map { it.move(100) }
             .count { q.contains(it) }
         }.reduce { a, b -> a * b }
 
-    private fun List<Robot>.move(times: Int) = map { it.move(times) }
-
     private fun Robot.move(times: Int): Robot {
-        fun wrap(max: Int, pos: Int, diff: Int) = ((pos + diff) % max + max) % max
-
         val newCol = wrap(COLUMNS, position.col, times * velocity.dx)
         val newRow = wrap(ROWS, position.row, times * velocity.dy)
         return Robot(Position(Point(newCol, newRow)), velocity)
@@ -48,7 +45,7 @@ class Day14(private var robots: List<Robot>) {
 
     private fun Quadrant.contains(robot: Robot): Boolean =
         robot.position.col in columnIndices &&
-                robot.position.row in rowIndices
+        robot.position.row in rowIndices
 
     private fun quadrants(): List<Quadrant> {
         val medianCol = COLUMNS / 2
@@ -81,25 +78,4 @@ class Day14(private var robots: List<Robot>) {
             )
         }
     }
-}
-
-fun main() {
-    val puzzle = Day14.using(
-        """
-        p=0,4 v=3,-3
-        p=6,3 v=-1,-3
-        p=10,3 v=-1,2
-        p=2,0 v=2,-1
-        p=0,0 v=1,3
-        p=3,0 v=-2,-2
-        p=7,6 v=-1,-3
-        p=3,0 v=-1,-2
-        p=9,3 v=2,3
-        p=7,3 v=-1,2
-        p=2,4 v=2,-3
-        p=9,5 v=-3,-3        
-        """.trimIndent().lines()
-    )
-
-    check(puzzle.part1() == 0)
 }
