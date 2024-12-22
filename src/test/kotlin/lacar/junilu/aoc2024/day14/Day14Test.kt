8f09d9c4ee510d951a4ddf7cc5901ae3cedcb5af
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-12-22 02:55:43 -0500

    t++ - Add for part 1 example, 2024 Day 14

--- src/test/kotlin/lacar/junilu/aoc2024/day14/Day14Test.kt
+++ src/test/kotlin/lacar/junilu/aoc2024/day14/Day14Test.kt
@@ -17,6 +17,27 @@ class Day14Test {
         assertEquals(7344, usingGitHubInput.part2())
     }
 
+    @Test
+    fun `Example - Part 1`() {
+        val input = """
+            p=0,4 v=3,-3
+            p=6,3 v=-1,-3
+            p=10,3 v=-1,2
+            p=2,0 v=2,-1
+            p=0,0 v=1,3
+            p=3,0 v=-2,-2
+            p=7,6 v=-1,-3
+            p=3,0 v=-1,-2
+            p=9,3 v=2,3
+            p=7,3 v=-1,2
+            p=2,4 v=2,-3
+            p=9,5 v=-3,-3            
+            """.trimIndent().lines()
+
+        val example = Day14.using(input, columns = 11, rows = 7)
+        assertEquals(12, example.part1())
+    }
+
     companion object {
         val usingGmailInput = Day14.using(readPuzzleInput("aoc2024/day14-gm"))
         val usingGitHubInput = Day14.using(readPuzzleInput("aoc2024/day14-gh"))
