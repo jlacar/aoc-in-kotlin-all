caa0bc3d6c4f5ef81f7a1e0819647e07bcc4dc67
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2025-12-04 08:42:35 -0500

    . t 2025 Day 4 all tests pass, including solutions for both parts.

---
+++ src/test/kotlin/lacar/junilu/aoc2025/day04/Day04Test.kt
@@ -0,0 +1,37 @@
+package lacar.junilu.aoc2025.day04
+
+import lacar.junilu.readPuzzleInput
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day04Test {
+
+    @Test
+    fun `Github input solutions`() {
+        val puzzle = Day04(readPuzzleInput("aoc2025/day04-gh"))
+
+        assertEquals(1395, puzzle.part1())
+        assertEquals(8451, puzzle.part2())
+    }
+
+    @Test
+    fun `Examples - Parts 1 & 2`() {
+        val example = Day04(
+            """
+            ..@@.@@@@.
+            @@@.@.@.@@
+            @@@@@.@.@@
+            @.@@@@..@.
+            @@.@@@@.@@
+            .@@@@@@@.@
+            .@.@.@.@@@
+            @.@@@.@@@@
+            .@@@@@@@@.
+            @.@.@@@.@.""".trimIndent().lines()
+        )
+
+        assertEquals(13, example.part1())
+        assertEquals(43, example.part2())
+    }
+
+}
