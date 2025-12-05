6558fc0fbc4d45b8967ce32a6d3cb9f17b982288
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2025-12-05 12:37:08 -0500

    . r Extract formula to extension function LongRange.count().
    Using the standard Iterable<T>.count() doesn't work in this case because of Int (actual) versus Long (required) return type. A private extension function with the same name preserves semantics while getting around this shortcoming.

--- src/main/kotlin/lacar/junilu/aoc2025/day05/Day05.kt
+++ src/main/kotlin/lacar/junilu/aoc2025/day05/Day05.kt
@@ -4,5 +4,7 @@ import lacar.junilu.common.combineOverlapping
 
 class Day05(val ranges: List<LongRange>, val ids: List<Long>) {
     fun part1(): Int = ids.count { id -> ranges.any { id in it } }
-    fun part2(): Long = ranges.combineOverlapping().sumOf { it.last - it.first + 1 }
+    fun part2(): Long = ranges.combineOverlapping().sumOf { it.count() }
 }
+
+private fun LongRange.count() = endInclusive - start + 1
