fc7c150a012243e2b7c788a73de6361e02ee9966
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2025-12-05 02:22:16 -0500

    . T 2025 Day 5 solved with all tests passing.

---
+++ src/test/kotlin/lacar/junilu/aoc2025/day05/Day05Test.kt
@@ -0,0 +1,36 @@
+package lacar.junilu.aoc2025.day05
+
+import lacar.junilu.readPuzzleInput
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day05Test {
+
+    @Test
+    fun `Example - Part 1 & 2`() {
+        val input = """
+            3-5
+            10-14
+            16-20
+            12-18
+
+            1
+            5
+            8
+            11
+            17
+            32""".trimIndent()
+
+        val example = Day05.using(input.lines())
+
+        assertEquals(3, example.part1())
+        assertEquals(14, example.part2())
+    }
+
+    @Test
+    fun `Github input solutions`() {
+        val puzzle = Day05.using(readPuzzleInput("aoc2025/day05-gh"))
+
+        assertEquals(840, puzzle.part1())
+        assertEquals(359913027576322, puzzle.part2())    }
+}
