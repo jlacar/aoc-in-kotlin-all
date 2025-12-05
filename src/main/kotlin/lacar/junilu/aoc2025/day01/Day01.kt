9cb2c25ce585dab8c7e501ca898e16eb6ffac7b7
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2025-12-04 23:48:07 -0500

    . r Simplify the fold operations and make parts 1 and 2 more symmetrical.

--- src/main/kotlin/lacar/junilu/aoc2025/day01/Day01.kt
+++ src/main/kotlin/lacar/junilu/aoc2025/day01/Day01.kt
@@ -10,17 +10,23 @@ import kotlin.math.abs
  */
 class Day01(val offsets: List<Int>) {
 
+    private val Pair<Int, Int>.zeroes get() = second
+
     private val startingPoint = 50
 
-    fun solvePart1() = offsets.fold(listOf(startingPoint)) { acc, offset ->
-        acc + nextPoint(acc.last(), offset)
-    }.count { it == 0 }
+    fun solvePart1() = offsets.fold(Pair(startingPoint, 0)) { acc, offset ->
+        val (current, zeroes) = acc
+        val next = nextPoint(current, offset)
+        next to zeroes + if (next == 0) 1 else 0)
+    }.zeroes
 
-    fun solvePart2() = offsets.fold(listOf(Pair(startingPoint, 0))) { acc, offset ->
-        acc + nextPointWithZeroCount(acc.last().first, offset)
-    }.sumOf { it.second }
+    fun solvePart2() = offsets.fold(Pair(startingPoint, 0)) { acc, offset ->
+        val (current, zeroes) = acc
+        val next = nextPoint(current, offset)
+        next to zeroes + zeroCount(current, offset))
+    }.zeroes
 
-    private fun nextPoint(current: Int, offset: Int) = wrap(max=100, pos=current, diff=offset)
+    private fun nextPoint(current: Int, offset: Int) = wrap(max = 100, pos = current, diff = offset)
 
     private fun nextPointWithZeroCount(current: Int, offset: Int) =
         Pair(nextPoint(current, offset), zeroCount(current, offset))
@@ -31,16 +37,15 @@ class Day01(val offsets: List<Int>) {
             return Day01(input.map { toOffset(it) })
         }
 
+        // Implementation note: this is here instead of being private so it can be tested
         fun zeroCount(current: Int, offset: Int): Int {
-            val clicks = abs(offset)
             val turningLeft = offset < 0
+            val clicks = abs(offset)
             val clicksToZero = if (current == 0) 100 else if (turningLeft) current else 100 - current
-            return when {
-                clicksToZero > clicks -> 0
-                else -> 1 + when {
-                    turningLeft -> (clicks - clicksToZero) / 100
-                    else -> (offset - clicksToZero) / 100
-                }
+            return if (clicksToZero > clicks) 0
+            else 1 + when {
+                turningLeft -> (clicks - clicksToZero) / 100
+                else -> (offset - clicksToZero) / 100
             }
         }
     }
