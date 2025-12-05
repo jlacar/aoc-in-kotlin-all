056b512981e3ae280d872ac51d0d95acd67e94c9
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2025-12-05 07:52:54 -0500

    . r Move the companion object with input parsing logic from the puzzle class to the test class.

--- src/main/kotlin/lacar/junilu/aoc2025/day05/Day05.kt
+++ src/main/kotlin/lacar/junilu/aoc2025/day05/Day05.kt
@@ -3,20 +3,6 @@ package lacar.junilu.aoc2025.day05
 import lacar.junilu.common.combineOverlapping
 
 class Day05(val ranges: List<LongRange>, val ids: List<Long>) {
-
     fun part1(): Int = ids.count { id -> ranges.any { id in it } }
     fun part2(): Long = ranges.combineOverlapping().sumOf { it.last - it.first + 1 }
-
-    companion object {
-        fun using(lines: List<String>): Day05 {
-            val blank = lines.indexOfFirst { it.isBlank() }
-            return Day05(idRangesFrom(lines.take(blank)), idsFrom(lines.drop(blank + 1)))
-        }
-
-        private fun idsFrom(lines: List<String>): List<Long> =
-            lines.map { it.toLong() }
-
-        private fun idRangesFrom(lines: List<String>): List<LongRange> =
-            lines.map { it.split("-").map { it.toLong() }.let { (start, end) -> start..end } }
-    }
 }
--- src/test/kotlin/lacar/junilu/aoc2025/day05/Day05Test.kt
+++ src/test/kotlin/lacar/junilu/aoc2025/day05/Day05Test.kt
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class Day05Test {
-
     @Test
     fun `Example - Part 1 & 2`() {
         val input = """
@@ -21,7 +20,7 @@ class Day05Test {
             17
             32""".trimIndent()
 
-        val example = Day05.using(input.lines())
+        val example = using(input.lines())
 
         assertEquals(3, example.part1())
         assertEquals(14, example.part2())
@@ -29,8 +28,21 @@ class Day05Test {
 
     @Test
     fun `Github input solutions`() {
-        val puzzle = Day05.using(readPuzzleInput("aoc2025/day05-gh"))
+        val puzzle = using(readPuzzleInput("aoc2025/day05-gh"))
 
         assertEquals(840, puzzle.part1())
         assertEquals(359913027576322L, puzzle.part2())    }
+
+    companion object {
+        fun using(lines: List<String>): Day05 {
+            val blank = lines.indexOfFirst { it.isBlank() }
+            return Day05(idRangesFrom(lines.take(blank)), idsFrom(lines.drop(blank + 1)))
+        }
+
+        private fun idsFrom(lines: List<String>): List<Long> =
+            lines.map { it.toLong() }
+
+        private fun idRangesFrom(lines: List<String>): List<LongRange> =
+            lines.map { it.split("-").map { it.toLong() }.let { (start, end) -> start..end } }
+    }
 }
