1d28edf563fe792674f016250baab5441317d742
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2025-12-06 04:12:18 -0500

    . r Rename lambda parameters to product and n, respectively.

--- src/main/kotlin/lacar/junilu/aoc2025/day06/Day06.kt
+++ src/main/kotlin/lacar/junilu/aoc2025/day06/Day06.kt
@@ -4,7 +4,7 @@ class Day06(val numbers: List<List<Long>>, val operations: List<String>) {
     fun solve(): Long = operations.mapIndexed { index, operation ->
         when (operation) {
             "+" -> numbers[index].sum()
-            "*" -> numbers[index].reduce { acc, i -> acc * i }
+            "*" -> numbers[index].reduce { product, n -> product * n }
             else -> throw IllegalArgumentException("Unknown operation $operation")
         }
     }.sum()
