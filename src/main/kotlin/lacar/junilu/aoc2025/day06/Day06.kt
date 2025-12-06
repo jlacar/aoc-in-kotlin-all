167f078c04fceffb8568f98112d42ee6b0601f25
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2025-12-06 10:33:01 -0500

    . d Add a note on where to find the input data parsing logic.

--- src/main/kotlin/lacar/junilu/aoc2025/day06/Day06.kt
+++ src/main/kotlin/lacar/junilu/aoc2025/day06/Day06.kt
@@ -1,5 +1,10 @@
 package lacar.junilu.aoc2025.day06
 
+/**
+ * AoC 2025: Day 06 - Trash Compactor
+ *
+ * https://adventofcode.com/2025/day/6
+ */
 class Day06(val numbers: List<List<Long>>, val operations: List<String>) {
     fun solve(): Long = operations.mapIndexed { index, operation ->
         when (operation) {
@@ -9,3 +14,11 @@ class Day06(val numbers: List<List<Long>>, val operations: List<String>) {
         }
     }.sum()
 }
+
+/*
+ * Developer Notes:
+ *
+ * 1. The crux of this problem is really the input parsing and shaping it into a format that's usable
+ *    in the code above. The parsing logic is in Day06Test.kt to keep the concern separated from the
+ *    calculations.
+ */
