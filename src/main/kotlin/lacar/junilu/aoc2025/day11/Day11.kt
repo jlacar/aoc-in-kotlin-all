930ca3639f7a3b65f1d064b6d7802eb7f8e56f4e
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2025-12-21 14:35:54 -0500

    . F 2025 Day 11 Part 1 solution completed, all tests passed.

--- src/main/kotlin/lacar/junilu/aoc2025/day11/Day11.kt
+++ src/main/kotlin/lacar/junilu/aoc2025/day11/Day11.kt
@@ -1,4 +1,15 @@
 package lacar.junilu.aoc2025.day11
 
-class Day11 {
+class Day11(val connections: Map<String, List<String>>) {
+
+    private val memo = mutableMapOf<String, Int>()
+
+    fun part1() = "you".pathsTo("out")
+
+    private fun String.pathsTo(other: String) = pathCount(this, other)
+
+    private fun pathCount(from: String, to: String): Int =
+        memo.getOrPut(from + to) {
+            if (from == to) 1 else connections[from]!!.sumOf { pathCount(it, to) }
+        }
 }
--- src/test/kotlin/lacar/junilu/aoc2025/day11/Day11Test.kt
+++ src/test/kotlin/lacar/junilu/aoc2025/day11/Day11Test.kt
@@ -1,13 +1,14 @@
 package lacar.junilu.aoc2025.day11
 
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
+import lacar.junilu.readPuzzleLines
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class Day11Test {
 
     @Test
     fun `Examples - Part 1 & 2`() {
-        val example = // using(
+        val example =
             """
             aaa: you hhh
             you: bbb ccc
@@ -19,6 +20,24 @@ class Day11Test {
             ggg: out
             hhh: ccc fff iii
             iii: out""".trimIndent().lines()
-        // )
+
+        val puzzle = using(example)
+
+        assertEquals(5, puzzle.part1())
     }
+
+    @Test
+    fun `GitHub input solution`() {
+        val puzzle = using(readPuzzleLines("aoc2025/day11-gh"))
+
+        assertEquals(500, puzzle.part1())
+    }
+
+    private fun using(input: List<String>) = Day11(
+        input.map {
+            it.split(": ").let { (fromDevice, toDevices) ->
+                fromDevice to toDevices.split(" ")
+            }
+        }.toMap()
+    )
 }
