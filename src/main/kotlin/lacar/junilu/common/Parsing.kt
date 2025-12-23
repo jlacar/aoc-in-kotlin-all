ada7949b16b57b22b8f063575061cedba2ab9f87
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2025-12-23 06:22:31 -0500

    . d Add region markers for parsing utilities

--- src/main/kotlin/lacar/junilu/common/Parsing.kt
+++ src/main/kotlin/lacar/junilu/common/Parsing.kt
@@ -1,6 +1,6 @@
 package lacar.junilu.common
 
-// region ===== Parsing input as numbers
+// region Parse as numbers
 
 private val ALL_NUMBERS = "[+-]?\\d+".toRegex()
 
@@ -10,8 +10,12 @@ fun String.findInts(): List<Int> = allNumbers().map { it.value.toInt() }.toList(
 
 fun String.findLongs(): List<Long> = allNumbers().map { it.value.toLong() }.toList()
 
-// region ===== Iterables and their elements
+// endregion
+
+// region Parse Iterables and their elements
 
 fun <T> Iterable<T>.dropEnds() = drop(1).dropLast(1)
 
 fun String.dropEnds() = drop(1).dropLast(1)
+
+// endregion
