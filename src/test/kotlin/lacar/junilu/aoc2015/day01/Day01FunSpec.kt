03605cd40f2b461a2cd43d5432da83c865eb9ead
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2026-01-07 09:04:02 -0500

    . t Add data-driven tests for FunSpec, AoC 2015 Day 1 examples.

--- src/test/kotlin/lacar/junilu/aoc2015/day01/Day01FunSpec.kt
+++ src/test/kotlin/lacar/junilu/aoc2015/day01/Day01FunSpec.kt
@@ -2,6 +2,7 @@ package lacar.junilu.aoc2015.day01
 
 import io.kotest.core.annotation.DisplayName
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 import lacar.junilu.readPuzzleText
 
@@ -16,4 +17,34 @@ class Day01FunSpec : FunSpec({
     test("Part 2 solution: position of instruction for first time in basement") {
         solution.firstPositionInBasement() shouldBe 1797
     }
+
+    context("Examples - Part 1") {
+        withData(
+            nameFn = { "given directions ${it.input} last floor should be ${it.expected}" },
+            TestData("(())", 0),
+            TestData("()()", 0),
+            TestData("(((", 3),
+            TestData("(()(()(", 3),
+            TestData("))(((((", 3),
+            TestData("())", -1),
+            TestData("))(", -1),
+            TestData(")))", -3),
+            TestData(")())())", -3),
+        ) { (directions, expectedFloor) ->
+            Day01(directions).lastFloor() shouldBe expectedFloor
+        }
+    }
+
+    context("Examples - Part 2") {
+        withData(
+            nameFn = { "given directions ${it.input} first position in basement should be ${it.expected}" },
+            TestData(")", 1),
+            TestData("()()))", 5),
+            TestData("()))())(())", 3)
+        ) { (directions, expectedPosition) ->
+            Day01(directions).firstPositionInBasement() shouldBe expectedPosition
+        }
+    }
 })
+
+internal data class TestData(val input: String, val expected: Int)
